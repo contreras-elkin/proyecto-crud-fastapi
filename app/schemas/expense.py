a8f140883590df94e668775c904c77833c47b17b
ElkinContrerasR <escontreras-2023a@corhuila.edu.co>
2025-12-10 20:49:41 -0500

    feat: agregar endpoint y esquema UPDATE

--- app/schemas/expense.py
+++ app/schemas/expense.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 
 class ExpenseCreate(BaseModel):
-    description: str = Field(..., min_length=1, description="La descripción es obligatoria")
+    description: str # = Field(..., min_length=1, description="La descripción es obligatoria")
     amount: float = Field(gt=0, description="El monto debe ser mayor a 0")
     category: str = Field(..., min_length=1, description="La categoría es obligatoria") 
 
@@ -13,3 +13,8 @@ class ExpenseResponse(BaseModel):
 
     class Config:
         from_attributes = True
+
+class ExpenseUpdate(BaseModel):
+    description: str | None=None#Field(None, min_length=1)
+    amount: float | None=Field(None, gt=0, description="El monto debe ser mayor a 0")
+    category: str | None=Field(None, min_length=1)
