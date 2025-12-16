b8f9bfa07358d28c6aaa3ac2ead21f299bcc72db
ElkinContrerasR <escontreras-2023a@corhuila.edu.co>
2025-12-16 12:25:34 -0500

    feat: agregar campo time en entidad expense

--- app/models/expense.py
+++ app/models/expense.py
@@ -1,11 +1,13 @@
 
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, Date
+from datetime import date
 from app.database import Base
 
 class Expense(Base):
     __tablename__ = "expenses"
 
     id = Column(Integer, primary_key=True, index=True)
-    description = Column(String(255), nullable=False)
+    description = Column(String(255), nullable=True)
     amount = Column(Float, nullable=False)
-    category = Column(String(50), nullable=False)
+    category = Column(String(50), nullable=True)
+    time = Column(Date, default=date.today, nullable=False)
